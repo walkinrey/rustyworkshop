f55cef2a3ce79a81f05986e751121199012deba1
walkinrey <andrushalight@gmail.com>
2021-07-18 13:14:16 +0500

    new features
    - added the ability to edit an existing icon
    - added main menu

---
+++ Assets/Editor/RustyWorkshopEditor.cs
@@ -0,0 +1,168 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(RustyWorkshop))]
+public class RustyWorkshopEditor : Editor
+{
+    public static GUIStyle RichStyle(bool middleCenter) 
+    {
+        var style = new GUIStyle();
+        style.richText = true;
+        if(middleCenter) style.alignment = TextAnchor.MiddleCenter;
+        return style;
+    }
+    public override void OnInspectorGUI()
+    {
+        var workshop = (RustyWorkshop)target;
+
+        GUILayout.Space(10f);
+
+        GUILayout.Label("<b><size=16><color=white>Start Interface Fields</color></size></b>", RichStyle(true));
+
+        GUILayout.Space(10f);
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Panel Object</color>", RichStyle(false));
+        workshop._panelStart = (GameObject)EditorGUILayout.ObjectField(workshop._panelStart, typeof(GameObject), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Status Text</color>", RichStyle(false));
+        workshop._statusTextStart = (Text)EditorGUILayout.ObjectField(workshop._statusTextStart, typeof(Text), true);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.Space(10f);
+
+        GUILayout.Label("<b><size=16><color=white>New Item Interface Fields</color></size></b>", RichStyle(true));
+
+        GUILayout.Space(10f);
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Panel Object</color>", RichStyle(false));
+        workshop._interfaceNewItem = (GameObject)EditorGUILayout.ObjectField(workshop._interfaceNewItem, typeof(GameObject), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Status Text</color>", RichStyle(false));
+        workshop._statusText = (Text)EditorGUILayout.ObjectField(workshop._statusText, typeof(Text), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Item Name Input Field</color>", RichStyle(false));
+        workshop._itemNameInput = (InputField)EditorGUILayout.ObjectField(workshop._itemNameInput, typeof(InputField), true);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Item Icon Input Field</color>", RichStyle(false));
+        workshop._itemIconInput = (InputField)EditorGUILayout.ObjectField(workshop._itemIconInput, typeof(InputField), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10f);
+
+        GUILayout.Label("<b><size=16><color=white>Update Item Interface Fields</color></size></b>", RichStyle(true));
+
+        GUILayout.Space(10f);
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Panel Object</color>", RichStyle(false));
+        workshop._interfaceUpdateItem = (GameObject)EditorGUILayout.ObjectField(workshop._interfaceUpdateItem, typeof(GameObject), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Status Text</color>", RichStyle(false));
+        workshop._statusTextUpdate = (Text)EditorGUILayout.ObjectField(workshop._statusTextUpdate, typeof(Text), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Header Item Icon Text</color>", RichStyle(false));
+        workshop._headerItemIconText = (Text)EditorGUILayout.ObjectField(workshop._headerItemIconText, typeof(Text), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Item ID Input Field</color>", RichStyle(false));
+        workshop._itemUpdateIDinput = (InputField)EditorGUILayout.ObjectField(workshop._itemUpdateIDinput, typeof(InputField), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Item Name Input Field</color>", RichStyle(false));
+        workshop._itemUpdateNameInput = (InputField)EditorGUILayout.ObjectField(workshop._itemUpdateNameInput, typeof(InputField), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Item Icon Input Field</color>", RichStyle(false));
+        workshop._itemUpdateIconInput = (InputField)EditorGUILayout.ObjectField(workshop._itemUpdateIconInput, typeof(InputField), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Item Changelog Input Field</color>", RichStyle(false));
+        workshop._itemUpdateChangelogInput = (InputField)EditorGUILayout.ObjectField(workshop._itemUpdateChangelogInput, typeof(InputField), true);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.Space(10f);
+
+        GUILayout.Label("<b><size=16><color=white>Notice Fields</color></size></b>", RichStyle(true));
+
+        GUILayout.Space(10f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Notice Panel Object</color>", RichStyle(false));
+        workshop._notice = (GameObject)EditorGUILayout.ObjectField(workshop._notice, typeof(GameObject), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Notice Footer Text Object</color>", RichStyle(false));
+        workshop._noticeFooterText = (GameObject)EditorGUILayout.ObjectField(workshop._noticeFooterText, typeof(GameObject), true);
+        GUILayout.EndHorizontal();
+    
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Notice Progress Bar Object</color>", RichStyle(false));
+        workshop._progressBar = (GameObject)EditorGUILayout.ObjectField(workshop._progressBar, typeof(GameObject), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Notice Text</color>", RichStyle(false));
+        workshop._noticeText = (Text)EditorGUILayout.ObjectField(workshop._noticeText, typeof(Text), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Notice Header Text</color>", RichStyle(false));
+        workshop._noticeHeaderText = (Text)EditorGUILayout.ObjectField(workshop._noticeHeaderText, typeof(Text), true);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5f);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("<color=white>Progress Bar Image</color>", RichStyle(false));
+        workshop._progressBarImage = (Image)EditorGUILayout.ObjectField(workshop._progressBarImage, typeof(Image), true);
+        GUILayout.EndHorizontal();
+    }
+}
--- Assets/RustyWorkshop.cs
+++ Assets/RustyWorkshop.cs
@@ -3,17 +3,16 @@ using UnityEngine;
 using Steamworks;
 using System.IO;
 using System;
-using System.Text;
 
 public class RustyWorkshop : MonoBehaviour
 {
-    [SerializeField] private GameObject _notice, _noticeFooterText, _progressBar;
-    [SerializeField] private Text _noticeText, _statusText, _noticeHeaderText;
-    [SerializeField] private InputField _itemNameInput, _itemIconInput;
-    [SerializeField] private Image _progressBarImage;
+    public GameObject _notice, _noticeFooterText, _progressBar, _interfaceNewItem, _interfaceUpdateItem, _panelStart;
+    public Text _noticeText, _statusText, _noticeHeaderText, _statusTextStart, _statusTextUpdate, _headerItemIconText;
+    public InputField _itemNameInput, _itemIconInput, _itemUpdateIDinput, _itemUpdateNameInput, _itemUpdateIconInput, _itemUpdateChangelogInput;
+    public Image _progressBarImage;
     private const string _manifest = @"{""Version"":3,""ItemType"":""Rock"",""AuthorId"":{0},""PublishDate"":""{1}"",""Groups"":[{""Textures"":{},""Floats"":{""_Cutoff"":0.0,""_BumpScale"":1.0,""_Glossiness"":1.0,""_OcclusionStrength"":1.0,""_DetailNormalMapScale"":0.65,""_DetailOcclusionStrength"":1.0,""_DetailOverlaySmoothness"":0.0,""_DetailOverlaySpecular"":0.0},""Colors"":{""_Color"":{""r"":1.0,""g"":1.0,""b"":1.0},""_SpecColor"":{""r"":1.0,""g"":1.0,""b"":1.0},""_EmissionColor"":{""r"":0.0,""g"":0.0,""b"":0.0},""_DetailColor"":{""r"":1.0,""g"":1.0,""b"":1.0}}}]}";
     private const string _status = "Steam: {0}\n\nНазвание предмета: {1}\nИконка: {2}\n\nСтатус: {3}";
-    private const string _faq = "<b>Что делать, если не получается подключиться к Steam</b>: попробуйте перезапустить Steam (вы обязательно должны быть авторизованы в нем) и RustyWorkshop, иных вариантов нет.\n\n<b>Требования к иконке</b>: должна весить не более 1 мегабайта и быть в формате изображения (не работает с .psd)";
+    private const string _faq = "<b>Что делать, если не получается подключиться к Steam</b>: попробуйте перезапустить Steam (вы обязательно должны быть авторизованы в нем) и RustyWorkshop.\n\n<b>Требования к иконке</b>: желательно должна быть размером 512x512, должна весить не более 1 мб и быть в формате изображения (.psd не поддерживается)";
     private bool _isInitialized = false;
     private bool _isAllReady = false;
     private void Start() 
@@ -33,56 +32,151 @@ public class RustyWorkshop : MonoBehaviour
     }
     public async void TryUpload() 
     {
-        if(!_isAllReady) 
+        if(_interfaceNewItem.activeInHierarchy) 
         {
-            ShowNotice("Исправьте все ошибки перед созданием иконки, они указаны в статусе!", "Ошибка");
+            if(!_isAllReady) 
+            {
+                ShowNotice("Исправьте все ошибки перед созданием иконки, они указаны в статусе!", "Ошибка");
+            }
+            else 
+            {
+                ShowNotice("Иконка загружается, пожалуйста подождите...", "Загрузка", true);
+
+                string newDirectoryPath = Application.dataPath + "/Temp";
+
+                if(Directory.Exists(newDirectoryPath)) 
+                {
+                    Directory.Delete(newDirectoryPath, true);
+                }
+
+                Directory.CreateDirectory(newDirectoryPath);
+
+                string newFilePath = $"{newDirectoryPath}/icon{Path.GetExtension(_itemIconInput.text)}";
+                File.Copy(_itemIconInput.text, newFilePath);
+
+                File.WriteAllText($"{newDirectoryPath}/manifest.txt", _manifest.Replace("{0}", Steamworks.SteamClient.SteamId.ToString()).Replace("{1}", DateTime.Now.ToString("o")));
+
+                System.Progress<float> progress = new System.Progress<float>(value => OnProgressChanged(value));
+
+                var task = Steamworks.Ugc.Editor.NewMicrotransactionFile
+                        .WithTitle(_itemNameInput.text)
+                        .WithDescription("RustyWorkshop - бесплатный, мультиплатформенный загрузчик иконок для Rust (только на rustyplugin.ru)")
+                        .WithContent(newDirectoryPath)
+                        .WithPreviewFile(newFilePath)
+                        .WithMetaData("")
+                        .ForAppId(252490)
+                        .WithPublicVisibility()
+                        .WithTag("Skin")
+                        .WithTag("Rock Skin")
+                        .WithTag("version2");
+                        
+                var result = await task.SubmitAsync(progress);        
+
+                if(result.Success) 
+                {
+                    ShowNotice($"Иконка была успешно загружена!\nID иконки в мастерской: {result.FileId}", "Иконка загружена");
+                    Application.OpenURL($"https://steamcommunity.com/sharedfiles/filedetails/?id={result.FileId}");
+                }
+                else 
+                {
+                    ShowNotice($"Не удалось загрузить иконку!\n{result.Result}", "Ошибка");
+                }
+
+                _progressBarImage.fillAmount = 0;
+                Directory.Delete(Application.dataPath + "/Temp", true);
+            }
         }
-        else 
+        if(_interfaceUpdateItem.activeInHierarchy) 
         {
-            ShowNotice("Иконка загружается, пожалуйста подождите...", "Загрузка", true);
+            if(!_isInitialized) 
+            {
+                ShowNotice("Вы не подключены к Steam!", "Ошибка");
+                return;
+            }
 
+            ulong id = 0;
+            try 
+            {
+                id = Convert.ToUInt64(_itemUpdateIDinput.text);
+            }
+            catch 
+            {
+                ShowNotice("ID введен неверно!", "Ошибка");
+                return;
+            }
+
+            ShowNotice("Иконка обновляется, пожалуйста подождите...", "Обновление", true);
+
+            bool isIconExists = false;
             string newDirectoryPath = Application.dataPath + "/Temp";
-            Directory.CreateDirectory(newDirectoryPath);
+            string newFilePath = "";
 
-            string newFilePath = $"{newDirectoryPath}/icon{Path.GetExtension(_itemIconInput.text)}";
-            File.Copy(_itemIconInput.text, newFilePath);
+            if(File.Exists(_itemUpdateIconInput.text))
+            {
+                if(Path.GetExtension(_itemUpdateIconInput.text) != ".psd") 
+                {
+                    isIconExists = true;
+                    newFilePath = $"{newDirectoryPath}/icon{Path.GetExtension(_itemUpdateIconInput.text)}";
+                }
+            }
+            else 
+            {
+                isIconExists = false;
+            }
 
-            File.WriteAllText($"{newDirectoryPath}/manifest.txt", _manifest.Replace("{0}", Steamworks.SteamClient.SteamId.ToString()).Replace("{1}", DateTime.Now.ToString("o")));
+            if(isIconExists) 
+            {
+                Directory.CreateDirectory(newDirectoryPath);
+
+                File.Copy(_itemUpdateIconInput.text, newFilePath);
+
+                File.WriteAllText($"{newDirectoryPath}/manifest.txt", _manifest.Replace("{0}", Steamworks.SteamClient.SteamId.ToString()).Replace("{1}", DateTime.Now.ToString("o")));
+            }
 
             System.Progress<float> progress = new System.Progress<float>(value =>
             {
                 OnProgressChanged(value);
             });
 
-            var task = Steamworks.Ugc.Editor.NewMicrotransactionFile
-                      .WithTitle(_itemNameInput.text)
-                      .WithDescription("RustyWorkshop - бесплатный, мультиплатформенный загрузчик иконок для Rust (только на rustyplugin.ru)")
-                      .WithContent(newDirectoryPath)
-                      .WithPreviewFile(newFilePath)
-                      .WithMetaData("")
-                      .ForAppId(252490)
-                      .WithPublicVisibility()
-                      .WithTag("Skin")
-                      .WithTag("Rock Skin")
-                      .WithTag("version2");
-                    
-            var result = await task.SubmitAsync(progress);        
+            var obj = new Steamworks.Ugc.Editor(id);
+
+            if(!string.IsNullOrEmpty(_itemUpdateNameInput.text))     
+            {
+                obj.WithTitle(_itemUpdateNameInput.text);
+            }   
+
+            obj.WithDescription("RustyWorkshop - бесплатный, мультиплатформенный загрузчик иконок для Rust (только на rustyplugin.ru)");  
+            obj.WithChangeLog(_itemUpdateChangelogInput.text);
+            obj.WithMetaData("");
+            obj.ForAppId(252490);
+            obj.WithPublicVisibility();
+            obj.WithTag("Skin");
+            obj.WithTag("Rock Skin");
+            obj.WithTag("version2");
+
+            if(isIconExists)
+            {
+                obj.WithContent(newDirectoryPath);
+                obj.WithPreviewFile(newFilePath);
+            }
+
+            var result = await obj.SubmitAsync(progress);            
 
             if(result.Success) 
             {
-                ShowNotice($"Иконка была успешно загружена!\nID иконки в мастерской: {result.FileId}", "Иконка загружена");
+                ShowNotice($"Иконка была успешно обновлена!\nID иконки в мастерской: {result.FileId}", "Иконка обновлена");
                 Application.OpenURL($"https://steamcommunity.com/sharedfiles/filedetails/?id={result.FileId}");
             }
             else 
             {
-                ShowNotice($"Не удалось загрузить иконку!\n{result.Result}", "Ошибка");
+                ShowNotice($"Не удалось обновить иконку!\n{result.Result}", "Ошибка");
             }
 
             _progressBarImage.fillAmount = 0;
-            Directory.Delete(Application.dataPath + "/Temp", true);
+            if(isIconExists) Directory.Delete(Application.dataPath + "/Temp", true);
         }
     }
-    private void OnProgressChanged(float progress) => _progressBarImage.fillAmount = progress;
+    private void OnProgressChanged(float progress, bool isUpdate = false) => _progressBarImage.fillAmount = progress;
     private void ShowNotice(string text, string header, bool enableProgressBar = false) 
     {
         _notice.SetActive(true);
@@ -93,73 +187,152 @@ public class RustyWorkshop : MonoBehaviour
         _noticeFooterText.SetActive(!enableProgressBar);
         _progressBar.SetActive(enableProgressBar);
     }
+    public void OpenItemCreation() 
+    {
+        _panelStart.SetActive(false);
+        _interfaceNewItem.SetActive(true);
+
+        UpdateStatus();
+    }
+    public void OpenItemUpdater() 
+    {
+        _panelStart.SetActive(false);
+        _interfaceUpdateItem.SetActive(true);
+
+        UpdateStatus();
+    }
+    public void BackToMenu() 
+    {
+        _interfaceUpdateItem.SetActive(false);
+        _interfaceNewItem.SetActive(false);
+        _panelStart.SetActive(true);
+        _notice.SetActive(false);
+
+        UpdateStatus();
+    }
     private void OnApplicationQuit() => SteamClient.Shutdown();
     public void UpdateStatus() 
     {
-        string status = _status;
+        if(_panelStart.activeInHierarchy) 
+        {
+            string status = "Steam: {0}";
 
-        bool isNameEntered = false;
-        bool isIconReady = false;
-
-        if(_isInitialized) 
-        {
-            status = status.Replace("{0}", "<color=green>подключен</color>");
-        }
-        else 
-        {   
-            status = status.Replace("{0}", "<color=red>не подключен</color>");
-        }
-
-        if(string.IsNullOrEmpty(_itemNameInput.text)) 
-        {
-            status = status.Replace("{1}", "<color=red>не указано</color>");
-            isNameEntered = false;
-        }
-        else 
-        {
-            status = status.Replace("{1}", "<color=green>указано</color>");
-            isNameEntered = true;
-        }
-
-        if(string.IsNullOrEmpty(_itemIconInput.text)) 
-        {
-            status = status.Replace("{2}", "<color=red>не найдена</color>");
-            isIconReady = false;
-        }
-        else 
-        {
-            if(File.Exists(_itemIconInput.text))
+            if(_isInitialized) 
             {
-                if(Path.GetExtension(_itemIconInput.text) != ".png") 
+                status = status.Replace("{0}", "<color=green>подключен</color>");
+            }
+            else 
+            {   
+                status = status.Replace("{0}", "<color=red>не подключен</color>");
+            }
+
+            _statusTextStart.text = status;
+        }
+        else 
+        {
+            if(_interfaceNewItem.activeInHierarchy) 
+            {
+                string status = _status;
+
+                bool isNameEntered = false;
+                bool isIconReady = false;
+
+                if(_isInitialized) 
                 {
-                    status = status.Replace("{2}", "<color=red>должна быть в формате PNG!</color>");
+                    status = status.Replace("{0}", "<color=green>подключен</color>");
+                }
+                else 
+                {   
+                    status = status.Replace("{0}", "<color=red>не подключен</color>");
+                }
+
+                if(string.IsNullOrEmpty(_itemNameInput.text)) 
+                {
+                    status = status.Replace("{1}", "<color=red>не указано</color>");
+                    isNameEntered = false;
+                }
+                else 
+                {
+                    status = status.Replace("{1}", "<color=green>указано</color>");
+                    isNameEntered = true;
+                }
+
+                if(string.IsNullOrEmpty(_itemIconInput.text)) 
+                {
+                    status = status.Replace("{2}", "<color=red>не найдена</color>");
                     isIconReady = false;
                 }
                 else 
                 {
-                    status = status.Replace("{2}", "<color=green>готова</color>");
-                    isIconReady = true;
+                    if(File.Exists(_itemIconInput.text))
+                    {
+                        if(Path.GetExtension(_itemIconInput.text) == ".psd") 
+                        {
+                            status = status.Replace("{2}", "<color=red>должна быть в формате изображения, .psd не поддерживается!</color>");
+                            isIconReady = false;
+                        }
+                        else 
+                        {
+                            status = status.Replace("{2}", "<color=green>готова</color>");
+                            isIconReady = true;
+                        }
+                    }
+                    else 
+                    {
+                        status = status.Replace("{2}", "<color=red>не найдена</color>");
+                        isIconReady = false;
+                    }
+                }
+
+                if(_isInitialized && isNameEntered && isIconReady) 
+                {
+                    status = status.Replace("{3}", "<color=green>можно создавать иконку</color>");
+                    _isAllReady = true;
+                }
+                else 
+                {
+                    status = status.Replace("{3}", "<color=red>имеются ошибки</color>");
+                    _isAllReady = false;
+                }
+
+                _statusText.text = status;
+            }
+
+            if(_interfaceUpdateItem.activeInHierarchy) 
+            {
+                if(_isInitialized) 
+                {
+                    _statusTextUpdate.text = "Steam: <color=green>подключен</color>";
+                }
+                else 
+                {   
+                    _statusTextUpdate.text = "Steam: <color=red>не подключен</color>";
+                }
+
+                if(string.IsNullOrEmpty(_itemUpdateIconInput.text))
+                {
+                    _headerItemIconText.text = "Иконка предмета (путь до файла)";
+                }
+                else
+                {
+                    if(File.Exists(_itemUpdateIconInput.text))
+                    {
+                        if(Path.GetExtension(_itemUpdateIconInput.text) == ".psd") 
+                        {
+                            _headerItemIconText.text = "<color=red>Иконка предмета (путь до файла)</color>";
+                        }
+                        else 
+                        {
+                            _headerItemIconText.text = "<color=green>Иконка предмета (путь до файла)</color>";
+                        }
+                    }
+                    else 
+                    {
+                        _headerItemIconText.text = "<color=red>Иконка предмета (путь до файла)</color>";
+                    }
                 }
             }
-            else 
-            {
-                status = status.Replace("{2}", "<color=red>не найдена</color>");
-                isIconReady = false;
-            }
         }
-
-        if(_isInitialized && isNameEntered && isIconReady) 
-        {
-            status = status.Replace("{3}", "<color=green>можно создавать иконку</color>");
-            _isAllReady = true;
-        }
-        else 
-        {
-            status = status.Replace("{3}", "<color=red>имеются ошибки</color>");
-            _isAllReady = false;
-        }
-
-        _statusText.text = status;
     }
     public void TryReconnect() => Start();
     public void ShowFAQ() => ShowNotice(_faq, "RustyWorkshop: FAQ");
